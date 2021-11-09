4bac8b8d7404de0e0398cbcdc26739b165a9367c
sergey1701 <73608392+sergey1701@users.noreply.github.com>
2021-11-09 11:46:36 +0200

    new

--- test.py
+++ test.py
@@ -7,3 +7,10 @@ class Person():
 
     def view_personal_data(self):
         print(self.first_name, self.last_name, self.age, self.gender)
+
+class Worker(Person):
+    def __init__(self,first_name, last_name, age, gender,  worker_id, payment):
+        self.worker_id = worker_id
+        self.payment = payment
+        super().__init__(first_name=first_name, last_name=last_name, age=age, gender=gender)
+        
